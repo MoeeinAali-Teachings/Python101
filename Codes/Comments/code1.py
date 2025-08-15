2a8531ad3b31a116efb109cd475f85bff22f8959
Moeein Aali <moeeeinaali@gmail.com>
2025-08-15 10:40:39 +0330

    [ADD] comment codes

---
+++ Codes/Comments/code1.py
@@ -0,0 +1,27 @@
+# this is a comment
+print("moeein")
+
+print("moeein aali") # this is my name
+
+
+# line1
+# line2
+# line3
+
+"""
+line 1 comment
+line 2 comment
+line 3 comment
+line 3 comment
+line 3 comment
+line 3 comment
+line 3 comment
+line 3 comment
+"""
+
+
+
+a = 2
+
+
+b = 3
